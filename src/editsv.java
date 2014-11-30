356d3f14b15b19623058046073c5dd2d8deac0b2
tohai <tohaia2k47@gmail.com>
2014-11-30 23:42:18 +0700

    Update editsv.java
    sua doi file edit cua hungnight

--- src/editsv.java
+++ src/editsv.java
@@ -1,5 +1,3 @@
-package ktx;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -7,14 +5,14 @@ import java.sql.Statement;
 
 public class editsv {
 	Connection c=new getconect().getcon();
-public editsv(String name,String lop,String mssv,String que,String ngaysinh,int dt,String khoa,int gt ,String maph){
+public editsv(String mssvc,String name,String lop,String mssv,String que,String ngaysinh,int dt,String khoa,int gt ,String maph){
 	String gioitinh;
 	if(gt==0)gioitinh="nam";
 	else gioitinh="nu";
 	String doituong="0"+dt;
 	try {
 	Statement stmt =c.createStatement();
-	String sql="update sinhvien set mssv='"+mssv+"',ten='"+name+"',quequan='"+que+"',lop='"+lop+"',ngaysinh='"+ngaysinh+"',gioitinh='"+gioitinh+"',maph='"+maph+"',khoa='"+khoa+"',doituong='"+doituong+"';";				
+	String sql="update sinhvien set mssv='"+mssv+"',ten='"+name+"',quequan='"+que+"',lop='"+lop+"',ngaysinh='"+ngaysinh+"',gioitinh='"+gioitinh+"',maph='"+maph+"',khoa='"+khoa+"',doituong='"+doituong+"'where mssv='"+mssvc+"';";				
 	stmt.executeUpdate(sql);    
     stmt.close();
     c.close();
