7bc9122d1ac79ca8f99c1d4119760c37d005829a
tohai <tohaia2k47@gmail.com>
2014-12-15 14:04:09 +0700

    bo sung bai cua trang,(*)anh huong cua viec doi phong

--- src/editphong.java
+++ src/editphong.java
@@ -15,19 +15,34 @@ import javax.swing.JOptionPane;
  */
 public class editphong {
     Connection c=new getconnect().getcon();
-    public editphong(int khu,String loaicu,String loaiphmoi,String maph_cu,String maph_moi)
-    {  int songuoi;
-    songuoi=0;
+    public editphong(String maphcu,String maphmoi,int loaiphong)
+    {  
+    stmt=c.createStatement();
+	String sql;
+	String loai;
+	if(loaiphong==1)loai="nam";
+	else if(loaiphong==2)loai="nu";
+	else {
+		JOptionPane.showMessageDialog(null, "ban hay chon loai phong cho ro rang");
+		return;
+		}
         try {
 	Statement stmt =c.createStatement();
-	String sql="update phong set ma_nha='"+khu+"',songuoi='"+songuoi+"',maph='"+maph_moi+"',loai_ph='"+loaiphmoi+"' where ma_nha='"+khu+"'and maph='"+maph_cu+"' and loai_ph='"+loaicu+"';";			
+	sql="update phong set maph='"+maphmoi+"',songuoi=0,loaiphong='"+loai+"' where maph='"+maphcu+"';";			
+	stmt.executeUpdate(sql);
+	sql="delete from sinhvien where maph='"+maphcu+"';";
+	stmt.executeUpdate(sql);
+	sql="update diennuoc set dtt =0 ,dts =0 ,ntt =0 ,nts =0 ,sum_sd =0 ,sum_nc =0 ,tiendien =0 ,tnuoc =0 "
+		+"where maph = '"+maphcu+"';";
+	stmt.executeUpdate(sql);
+	sql="update diennuoc set maph='"+maphmoi+"'where maph='"+maphcu+"';";
 	stmt.executeUpdate(sql);
 	JOptionPane.showMessageDialog(null, "successful");
     stmt.close();
     c.close();
 	//stmt.executeQuery(sql);
 } catch (SQLException e) {
-	// TODO Auto-generated catch block
+	JOptionPane.showMessageDialog(null, "failse in sql or instruct csdl");
 	e.printStackTrace();
 }
     }
